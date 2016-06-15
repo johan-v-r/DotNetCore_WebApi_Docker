d07a680093a758231f1dc88bf3ba943866866e29
JvR <Johan@KRS.local>
2016-06-15 13:15:44 +0200

    Project structure changes

--- Controllers/DetailsController.cs
+++ Controllers/DetailsController.cs
@@ -1,21 +1,18 @@
-using System.Net;
-using System.Net.Http;
 using Microsoft.AspNetCore.Mvc;
 
-[Route("api/values/{valueId}/details")]
-public class DetailsController : Controller
+namespace ConsoleApplication.Controllers
 {
-  [HttpGet]
-  public string Get(int valueId) {
-    return "get normal";
-  }
+  [Route("api/values/{valueId}/details")]
+  public class DetailsController : Controller
+  {
+    [HttpGet]
+    public string Get(int valueId) {
+      return "get normal";
+    }
 
-  [HttpGet("{id}")]
-  public string Get(int valueId, int id) {
-    return "get by id " + id;
-  }
-
-  public HttpResponseMessage Post() {
-    return new HttpResponseMessage(HttpStatusCode.OK, "test");
+    [HttpGet("{id}")]
+    public string Get(int valueId, int id) {
+      return "get by id " + id;
+    }
   }
 }
--- Startup.cs
+++ Startup.cs
@@ -3,20 +3,23 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-public class Startup
+namespace ConsoleApplication
 {
-  public Startup(IHostingEnvironment env) {
-    var builder = new ConfigurationBuilder();
-    Configuration = builder.Build();
-  }
+  public class Startup
+  {
+    public Startup(IHostingEnvironment env) {
+      var builder = new ConfigurationBuilder();
+      Configuration = builder.Build();
+    }
 
-  public IConfigurationRoot Configuration { get; }
+    public IConfigurationRoot Configuration { get; }
 
-  public void ConfigureServices(IServiceCollection services) {
-    services.AddMvc();
-  }
+    public void ConfigureServices(IServiceCollection services) {
+      services.AddMvc();
+    }
 
-  public void Configure(IApplicationBuilder app) {
-    app.UseMvc();
-  }
+    public void Configure(IApplicationBuilder app) {
+      app.UseMvc();
+    }
+  } 
 }
